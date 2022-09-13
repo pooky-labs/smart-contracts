fc878848da8f087ed0cbcddbfff06f46758f0dca
dusan <duxxud@gmail.com>
2022-09-13 03:38:48 +0200

    IPook rename to IPOK

--- contracts/interfaces/IPOK.sol
+++ contracts/interfaces/IPOK.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-interface IPook {
+interface IPOK {
     function mint(address to, uint256 amount) external;
 }
--- contracts/mocks/MockPOK.sol
+++ contracts/mocks/MockPOK.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-import "../Pook.sol";
+import "../POK.sol";
 
-contract MockPook is Pook {
+contract MockPOK is POK {
     function mock_mint(address to, uint256 amount) external {
         _mint(to, amount);
     }
