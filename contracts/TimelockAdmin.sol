9c093b0d8ff3a99272fedd2f9fa0524f96e86b45
dusan <duxxud@gmail.com>
2022-11-03 02:25:52 +0100

    docs: added Timelock natspec; added roles and wallets descriptions in readme

--- contracts/TimelockAdmin.sol
+++ contracts/TimelockAdmin.sol
@@ -9,6 +9,13 @@ import "@openzeppelin/contracts/governance/TimelockController.sol";
  * @title TimelockAdmin
  * @author Pooky Engineering Team
  *
+ * @notice This contract will be used as ProxyAdmin on proxy contracts 
+ * and as DEFAULT_ADMIN_ROLE in implementation contracts.
+ * It's standard OpenZepellin implementation of Timelock contract in which 
+ * each action needs to be proposed and waited for `minDelay` time until exectued.
+ *
+ * `proposers` will be set to Pooky Tech Team multisig wallet and Pooky Executives Team multisig.
+ * `executors` will be only Pooky Executives Team multisig.
  */
 contract TimelockAdmin is TimelockController {
 
