9f922b0f3eebeb73bf583bc157d4204a3c7e2b4f
Mathieu Bour <mathieu.bour@pooky.gg>
2022-11-03 14:33:19 +0100

    chore: remove typos

--- contracts/TimelockAdmin.sol
+++ contracts/TimelockAdmin.sol
@@ -9,10 +9,10 @@ import "@openzeppelin/contracts/governance/TimelockController.sol";
  * @title TimelockAdmin
  * @author Pooky Engineering Team
  *
- * @notice This contract will be used as ProxyAdmin on proxy contracts
- * and as DEFAULT_ADMIN_ROLE in implementation contracts.
- * It's standard OpenZepellin implementation of Timelock contract in which
- * each action needs to be proposed and waited for `minDelay` time until exectued.
+ * @notice This contract will be used as ProxyAdmin on proxy contracts and as DEFAULT_ADMIN_ROLE in implementation
+ * contracts.
+ * This contract is the standard OpenZeppelin implementation of Timelock contract in which each action needs to be
+ * proposed and waited for `minDelay` time until executed.
  *
  * `proposers` will be set to Pooky Tech Team multisig wallet and Pooky Executives Team multisig.
  * `executors` will be only Pooky Executives Team multisig.
