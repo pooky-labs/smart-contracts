9c8ff8485ec1d5c9673a79d784c9d146a5ad6123
Mathieu Bour <mathieu.bour@pooky.gg>
2023-01-16 15:31:16 +0100

    feat(rewards): ensure that rewards ids cannot be claimed more than once

--- lib/testing/signRewards.ts
+++ lib/testing/signRewards.ts
@@ -1,18 +1,16 @@
-import { BigNumberish, Signer, utils } from 'ethers';
+import { Signer, utils } from 'ethers';
 import { RewardsDataStruct } from '../../typechain-types/contracts/game/Rewards';
 
 /**
  * Sign the reward claim payload.
  * @param account The address of the user who is claiming the reward.
  * @param rewards The rewards object.
- * @param nonce The signature nonce.
  * @param data Arbitrary string to associate with the rewards.
  * @param signer The ethers.js signer.
  */
 export async function signRewards(
   account: string,
   rewards: Partial<RewardsDataStruct>,
-  nonce: BigNumberish,
   data: string,
   signer: Signer,
 ): Promise<[string, RewardsDataStruct]> {
@@ -20,17 +18,17 @@ export async function signRewards(
     amountNAT: 0,
     amountPOK: 0,
     pxp: [],
+    hashes: [],
     ...rewards,
   };
 
   const payload = utils.defaultAbiCoder.encode(
     [
       'address',
-      'tuple(uint256 amountNAT, uint256 amountPOK, tuple(uint256 tokenId, uint256 amountPXP)[] pxp)',
-      'uint256',
+      'tuple(uint256 amountNAT, uint256 amountPOK, tuple(uint256 tokenId, uint256 amountPXP)[] pxp, bytes32[] hashes)',
       'string',
     ],
-    [account, fullRewards, nonce, data],
+    [account, fullRewards, data],
   );
   const hash = utils.keccak256(payload);
   const signature = await signer.signMessage(utils.arrayify(hash));
