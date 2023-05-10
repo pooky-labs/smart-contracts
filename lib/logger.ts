96100032fbea227a3d709cb98d51f3d5607dfa93
Mathieu Bour <mathieu.bour@pooky.gg>
2023-05-10 16:59:56 +0200

    chore(foundry): drop legacy Hardhat config

--- lib/logger.ts
+++
@@ -1,7 +0,0 @@
-import { Logger } from 'tslog';
-
-const logger = new Logger({
-  prettyLogTemplate: '{{yyyy}}.{{mm}}.{{dd}} {{hh}}:{{MM}}:{{ss}}:{{ms}} {{logLevelName}}\t',
-});
-
-export default logger;
