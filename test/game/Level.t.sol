7797cedcaf9546ed8ee05b3b2e903f9fb4358518
Mathieu Bour <mathieu.bour@pooky.gg>
2023-05-19 16:33:28 +0200

    test: reach 100% line coverage

--- test/game/Level.t.sol
+++ test/game/Level.t.sol
@@ -10,7 +10,10 @@ import { POKSetup } from "../setup/POKSetup.sol";
 import { PookyballSetup } from "../setup/PookyballSetup.sol";
 
 contract LevelTest is Test, POKSetup, PookyballSetup {
+  address public admin = makeAddr("admin");
   address public user = makeAddr("user");
+  address public game = makeAddr("game");
+
   PookyballRarity[] public rarities = [
     PookyballRarity.COMMON,
     PookyballRarity.RARE,
@@ -23,6 +26,11 @@ contract LevelTest is Test, POKSetup, PookyballSetup {
 
   function setUp() public {
     level = new Level(pok, pookyball);
+
+    vm.startPrank(admin);
+    pok.grantRole(pok.BURNER(), address(level));
+    pookyball.grantRole(pookyball.GAME(), address(level));
+    vm.stopPrank();
   }
 
   function test_levelPXP() public {
@@ -80,7 +88,7 @@ contract LevelTest is Test, POKSetup, PookyballSetup {
     uint256 tokenId = mintPookyball(user, rarity);
     uint256 maxLevel = level.maxLevels(rarity);
 
-    vm.prank(makeAddr("game"));
+    vm.prank(game);
     pookyball.setLevel(tokenId, maxLevel);
     mintPOK(user, level.levelPOKCost(tokenId, 1));
 
@@ -92,13 +100,13 @@ contract LevelTest is Test, POKSetup, PookyballSetup {
     assertEq(currentLevel, maxLevel);
   }
 
-  function test_levelUp_revertsIfInsufficientPOK(uint256 currentLevel, uint8 rarity8) public {
+  function test_levelUp_revertInsufficientPOKBalance(uint256 currentLevel, uint8 rarity8) public {
     PookyballRarity rarity = randomPookyballRarity(rarity8);
     currentLevel = bound(currentLevel, 0, 39);
 
     uint256 tokenId = mintPookyball(user, rarity);
 
-    vm.prank(makeAddr("game"));
+    vm.prank(game);
     pookyball.setLevel(tokenId, currentLevel);
 
     uint256 pokCost = level.levelPOKCost(tokenId, 1);
@@ -111,12 +119,12 @@ contract LevelTest is Test, POKSetup, PookyballSetup {
     level.levelUp(tokenId, 1);
   }
 
-  function test_levelUp_revertIfMultipleLevelsWouldExceedMaximumLevel(uint8 rarity8) public {
+  function test_levelUp_revertMaximumLevelReached(uint8 rarity8) public {
     PookyballRarity rarity = randomPookyballRarity(rarity8);
     uint256 maxLevel = level.maxLevels(rarity);
     uint256 tokenId = mintPookyball(user, rarity);
 
-    vm.prank(makeAddr("game"));
+    vm.prank(game);
     pookyball.setLevel(tokenId, maxLevel - 2);
 
     mintPOK(user, level.levelPOKCost(tokenId, 3));
@@ -124,5 +132,37 @@ contract LevelTest is Test, POKSetup, PookyballSetup {
     vm.expectRevert(abi.encodeWithSelector(Level.MaximumLevelReached.selector, tokenId, maxLevel));
     vm.prank(user);
     level.levelUp(tokenId, 3);
+  }
+
+  function test_levelUp_passWithPXP(
+    uint8 rarity8,
+    uint256 startLevel,
+    uint256 deltaLevel,
+    uint256 totalPXP
+  ) public {
+    PookyballRarity rarity = randomPookyballRarity(rarity8);
+    uint256 tokenId = mintPookyball(user, rarity);
+    uint256 maxLevel = level.maxLevels(rarity);
+    startLevel = bound(startLevel, 0, maxLevel - 1);
+    deltaLevel = bound(deltaLevel, 1, maxLevel - startLevel);
+
+    vm.prank(game);
+    pookyball.setLevel(tokenId, startLevel);
+
+    uint256 requiredPXP = 0;
+    for (uint256 i = startLevel + 1; i < startLevel + deltaLevel; i++) {
+      requiredPXP += level.levelPXP(i);
+    }
+    vm.assume(totalPXP > requiredPXP);
+
+    vm.prank(game);
+    pookyball.setPXP(tokenId, totalPXP);
+
+    uint256 pricePOK = level.levelPOKCost(tokenId, deltaLevel);
+    mintPOK(user, pricePOK);
+
+    vm.prank(user);
+    level.levelUp(tokenId, deltaLevel);
+    assertEq(pookyball.metadata(tokenId).level, startLevel + deltaLevel);
   }
 }
