114570613a6bdc9710d234f64768c7a3d5319d54
Mathieu Bour <mathieu.bour@pooky.gg>
2023-05-26 09:09:51 +0200

    chore(config): configure Etherscan and Polygon/Mumbai RPCs

--- scripts/artifacts.ts
+++ scripts/artifacts.ts
@@ -1,19 +1,21 @@
-import glob from "fast-glob";
-import { exec } from "node:child_process";
-import { createWriteStream } from "node:fs";
-import { mkdir } from "node:fs/promises";
-import { basename, dirname } from "node:path";
+import glob from 'fast-glob';
+import { exec as execAsync } from 'node:child_process';
+import { mkdir, writeFile } from 'node:fs/promises';
+import { basename, dirname } from 'node:path';
+import { promisify } from 'util';
+const exec = promisify(execAsync);
 
 /**
  * Generate the contract json ABI.
  * @param contract The contract path.
  */
 async function abi(contract: string): Promise<void> {
-  const name = basename(contract).replace(/\.sol$/, "");
-  const output = contract.replace(/^src/, "abi").replace(/\.sol$/, ".json");
+  const name = basename(contract).replace(/\.sol$/, '');
+  const output = contract.replace(/^src/, 'abi').replace(/\.sol$/, '.json');
   await mkdir(dirname(output), { recursive: true });
-  const stream = createWriteStream(output);
-  exec(`forge inspect ${contract}:${name} abi`).stdout?.pipe(stream);
+
+  const { stdout } = await exec(`forge inspect ${contract}:${name} abi`);
+  await writeFile(output, stdout);
 }
 
 /**
@@ -21,16 +23,15 @@ async function abi(contract: string): Promise<void> {
  * @param contract The contract path.
  */
 async function bytecode(contract: string): Promise<void> {
-  const name = basename(contract).replace(/\.sol$/, "");
-  const output = contract.replace(/^src/, "bytecode").replace(/\.sol$/, ".bin");
+  const name = basename(contract).replace(/\.sol$/, '');
+  const output = contract.replace(/^src/, 'bytecode').replace(/\.sol$/, '.bin');
   await mkdir(dirname(output), { recursive: true });
-  const stream = createWriteStream(output);
-  exec(`forge inspect ${contract}:${name} bytecode`).stdout?.pipe(stream);
+
+  const { stdout } = await exec(`forge inspect ${contract}:${name} bytecode`);
+  await writeFile(output, stdout);
 }
 
-await exec;
-
-const contracts = await glob("src/**/*.sol");
+const contracts = await glob('src/**/*.sol');
 const jobs = contracts.reduce<Promise<void>[]>((acc, contract) => {
   acc.push(abi(contract), bytecode(contract));
   return acc;
