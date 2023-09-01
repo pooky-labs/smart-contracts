49f1ec4589f9a6555edfb35751cbc0f0c42b7eb3
Mathieu Bour <mathieu.bour@pooky.gg>
2023-09-01 11:31:47 +0200

    feat(energy): allow players to purchase energy points via an NFT

---
+++ src/common/Energy.sol
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+import { ERC721A } from "ERC721A/ERC721A.sol";
+import { ERC721ABurnable } from "ERC721A/extensions/ERC721ABurnable.sol";
+import { ERC721AQueryable } from "ERC721A/extensions/ERC721AQueryable.sol";
+import { IERC721A } from "ERC721A/IERC721A.sol";
+import { Ownable } from "solady/auth/Ownable.sol";
+import { Base64 } from "solady/utils/Base64.sol";
+import { LibString } from "solady/utils/LibString.sol";
+import { Treasury } from "@/common/Treasury.sol";
+
+/// @title Energy
+/// @author Mathieu Bour for Pooky Labs Ltd.
+/// Allow players to purchase a voucher representing energy on the Pooky.gg game.
+contract Energy is ERC721A, ERC721ABurnable, ERC721AQueryable, Ownable, Treasury {
+  using LibString for uint256;
+
+  /// How much cost 1 Energy point
+  uint256 public pricing;
+
+  /// How much Energy is worth each token
+  mapping(uint256 => uint256) public values;
+
+  constructor(address admin, address _treasury, uint256 _pricing)
+    ERC721A("Pooky Energy", "PERG")
+    Treasury(_treasury)
+  {
+    _initializeOwner(admin);
+    pricing = _pricing;
+  }
+
+  /// @notice Change the Energy pricing
+  function setPricing(uint256 _pricing) external onlyOwner {
+    pricing = _pricing;
+  }
+
+  /// @dev Voucher token IDs start at 1.
+  function _startTokenId() internal pure override returns (uint256) {
+    return 1;
+  }
+
+  /// @notice Generate the on-chain metadata
+  /// @dev Exepcted pattern:
+  /// ```
+  /// {
+  ///   "name": "Voucher {tokenId}",
+  ///   "description": "Voucher granting XX Energy on Pooky.gg",
+  ///   "attributes": [
+  ///     { "trait_type": "Value", "value": XX }
+  ///   ]
+  /// }
+  /// ```
+  /// The function is deliberatly gas-inefficient, but far more clear.
+  function tokenURI(uint256 tokenId)
+    public
+    view
+    override(ERC721A, IERC721A)
+    returns (string memory)
+  {
+    string memory value = values[tokenId].toString();
+    bytes memory dataURI = abi.encodePacked(
+      "{",
+      abi.encodePacked('"name": "Voucher #', tokenId.toString(), '",'),
+      abi.encodePacked('"description": "Voucher granting ', value, ' Energy on Pooky.gg",'),
+      abi.encodePacked(
+        '"attributes": [',
+        abi.encodePacked('{ "trait_type": "Value", "value": ', value, " }"), // Energy attribute
+        "]"
+      ),
+      "}"
+    );
+
+    return string(abi.encodePacked("data:application/json;base64,", Base64.encode(dataURI)));
+  }
+
+  /// Mint a new Energy voucher
+  /// @param quantity The amount of Energy points
+  /// @param recipient The address that should receive the voucher
+  /// @dev Requirements
+  /// - msg.value must be greater than pricing*quantity
+  function mint(uint256 quantity, address recipient)
+    external
+    payable
+    forwarder
+    returns (uint256 tokenId)
+  {
+    uint256 expected = quantity * pricing;
+
+    if (msg.value < expected) {
+      revert InsufficientValue(expected, msg.value);
+    }
+
+    tokenId = _nextTokenId();
+    _mint(recipient, 1);
+    values[tokenId] = quantity;
+  }
+}
---
+++ test/common/Energy.t.sol
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+import { Ownable } from "solady/auth/Ownable.sol";
+import { Base64 } from "solady/utils/Base64.sol";
+import { LibString } from "solady/utils/LibString.sol";
+import { Energy } from "@/common/Energy.sol";
+import { ITreasury } from "@/common/ITreasury.sol";
+import { BaseTest } from "@test/BaseTest.sol";
+
+struct Attribute {
+  string trait_type;
+  uint256 value;
+}
+
+struct Metadata {
+  Attribute[] attributes;
+  string description;
+  string name;
+}
+
+contract EnergyTest is BaseTest {
+  using LibString for uint256;
+
+  address admin = makeAddr("admin");
+  address treasury = makeAddr("treasury");
+  address player = makeAddr("player");
+
+  uint256 defaultPricing = 5.15 ether;
+
+  Energy energy;
+
+  function setUp() public {
+    energy = new Energy(admin, treasury, defaultPricing);
+  }
+
+  function test_setPricing_revertUnauthorized() public {
+    vm.prank(player);
+    vm.expectRevert(Ownable.Unauthorized.selector);
+    energy.setPricing(1);
+  }
+
+  function test_setPricing_pass() public {
+    uint256 newPricing = 7 ether;
+
+    assertEq(energy.pricing(), defaultPricing);
+    vm.prank(admin);
+    energy.setPricing(newPricing);
+    assertEq(energy.pricing(), newPricing);
+  }
+
+  function trim(string calldata str, uint256 start) external pure returns (string memory) {
+    return str[start:];
+  }
+
+  function test_tokenURI_pass() public {
+    uint256 quantity = 20;
+    uint256 tokenId = energy.mint{ value: quantity * energy.pricing() }(quantity, player);
+    string memory tokenIdStr = tokenId.toString();
+
+    string memory data64 =
+      LibString.replace(energy.tokenURI(tokenId), "data:application/json;base64,", "");
+    string memory decoded = string(Base64.decode(data64));
+
+    (Metadata memory metadata) = abi.decode(vm.parseJson(decoded), (Metadata));
+
+    assertEq(metadata.name, string(abi.encodePacked("Voucher #", tokenIdStr)));
+    assertEq(
+      metadata.description,
+      string(abi.encodePacked("Voucher granting ", quantity.toString(), " Energy on Pooky.gg"))
+    );
+    assertEq(metadata.attributes.length, 1);
+    assertEq(metadata.attributes[0].trait_type, "Value");
+    assertEq(metadata.attributes[0].value, quantity);
+  }
+
+  function test_mint_revertInsufficientValue() public {
+    uint256 quantity = 30;
+    uint256 expectedValue = quantity * energy.pricing();
+    uint256 actualValue = expectedValue - 1;
+
+    vm.expectRevert(
+      abi.encodeWithSelector(ITreasury.InsufficientValue.selector, expectedValue, actualValue)
+    );
+    hoax(player, expectedValue);
+    energy.mint{ value: actualValue }(quantity, player);
+  }
+
+  function test_mint_pass() public {
+    uint256 quantity = 25;
+    uint256 value = quantity * energy.pricing();
+
+    hoax(player, value);
+    uint256 tokenId = energy.mint{ value: value }(quantity, player);
+
+    assertEq(energy.values(tokenId), quantity);
+  }
+}
